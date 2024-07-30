f86858ee196a0970a34e2d4b4ad2380356004a13
Salih <imago_999@hotmail.com>
2024-07-30 13:54:14 +0300

    file.bin is created. It is for storing data.

--- src/EngTur.java
+++ src/EngTur.java
@@ -1,16 +1,22 @@
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 // From English to Turkish. exp. door = kapı (dor).
-public class EngTur implements Flashcard{
-    private static Map<String,String> words = new HashMap<>();
+public class EngTur implements Flashcard,Serializable{
+    private static Map<String,String> words;
     private int size = 0;
+    public EngTur(){
+        fromFile();
+    }
 
     @Override
     public void add(String engWord,String meaning) {
         String word = words.putIfAbsent(engWord,meaning);
         size++;
-        if(word == null) System.out.println(engWord + " already exists");
+        if(word != null) System.out.println(engWord + " already exists");
+        else toFile();
+
     }
     @Override
     public boolean delete(String engWord,String meaning) {
@@ -25,5 +31,22 @@ public class EngTur implements Flashcard{
     @Override
     public boolean search(String engWord) {
         return (words.containsKey(engWord));
+    }
+
+    private void toFile(){
+        try(ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("file.bin"))){
+                objectOutputStream.writeObject(words);
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+    private void fromFile(){
+        try(ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("file.bin"))){
+            words = (Map<String, String>) objectInputStream.readObject();
+        }catch (IOException ex){
+            ex.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        EngTur engTur = new EngTur();
+        System.out.println(engTur.search("door"));
+    }
 }
